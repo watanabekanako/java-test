6401ed189507e261ee68d5c2728344607e95d455
“Kanako_Watanabe” <109854906+watanabekanako@users.noreply.github.com>
2024-02-12 15:05:58 +0900

    commit

---
+++ Greeting.java
@@ -0,0 +1,7 @@
+// class
+class Greeting{
+                    // method
+                    public static void main(String[] args){
+System.out.println(" Morning");
+                    }
+}
---
+++ Lesson6.java
@@ -0,0 +1,10 @@
+class Lesson6 {
+    public static void main(String[] args) {
+String[] arr ;
+arr= new String[3];
+arr[0] = "Hello";
+arr[1] = "World";
+arr[2] = "!";
+        System.out.println(arr);
+    }
+}
